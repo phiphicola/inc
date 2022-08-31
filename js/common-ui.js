19e316aeb8bfe090aac48f3dd1cd325ab3970dee
phiphicola <dghmg85@gmail.com>
2022-09-01 08:46:24 +0900

    ytijt

--- js/common-ui.js
+++ js/common-ui.js
@@ -1,5 +1,4 @@
 $(function(){
-    favoriteBtn();
     clearBtn();
     tabs();
     fixedBg();
@@ -37,7 +36,7 @@ $(function(){
 
 // 버튼 선택 //삭제 될 수 있음
 $(function(){
-    $('.select .btn-round, .select-form li').on('click', function() {
+    $('.select button, .select-form li').on('click', function() {
         $(this).siblings().removeClass('on');
         $(this).addClass('on');
     });
@@ -45,7 +44,7 @@ $(function(){
 
 $(function(){
     var fixButton = $('.wrap-btn-sticky');
-    var elHeight = $('.notice-section').outerHeight();
+    var elHeight = $('.notice-wrap').outerHeight();
     $(window).scroll(function() {
         var scroll = $(window).scrollTop();
         var val = $(document).height() - $(window).height() - elHeight;
@@ -59,17 +58,18 @@ $(function(){
 
 $(function(){
     var blurTitle = $('.header-title-left.blur');
-    var hederHeight = $('header').outerHeight() - 30;
+    var hederHeight = $('header').outerHeight();
     $(window).scroll(function() {
         var scroll = $(window).scrollTop();
         var val = hederHeight;
-        if  (scroll >= val) {
-            blurTitle.fadeIn();
-        }else {
-            blurTitle.fadeOut();
-        }
+        var valPercent = (scroll / val);
+        // console.log(valPercent)
+            blurTitle.css({
+                "opacity" : valPercent
+            });
     });
 });
+
 
 
 $(function(){
@@ -84,8 +84,7 @@ $(function(){
 });
 
 $(function(){
-    var fixBtn = $('.wrap-btn-fixed');
-    
+    var fixBtn = $('.wrap-btn-fixed');    
     $('.input-type').on('focus', function() {
         fixBtn.addClass('sticky');
     })
@@ -94,15 +93,6 @@ $(function(){
     })
     
 });
-
-
-
-// 관심종목 등록
-function favoriteBtn () {
-    $('.favorite button').on('click', function() {
-        $(this).toggleClass('on');
-     });
-}
 
 // 검색 삭제
 function clearBtn () {
@@ -294,7 +284,7 @@ function bottomSheet() {
             window.addEventListener('touchend', onDragEnd)
         })
         
-        jQuery('.sheet .dim').on('click', function (){            
+        jQuery('.sheet .dim').on('click', function () {
             setIsSheetShow(false)
             document.body.classList.remove('hidden')
         })
@@ -306,7 +296,7 @@ function bottomSheet() {
 
         const onDragStart = (event) => {
             dragPosition = touchPosition(event).pageY
-            $targetSheetContents.addClass('not-selectable');
+            $targetSheetContents.addClass('not-selectable')
             draggableArea.style.cursor = document.body.style.cursor = 'grabbing'
         }
 
@@ -339,120 +329,3 @@ function bottomSheet() {
         
     }
 }
-
-
-// 리스트 드래그
-// function listDrag() {
-
-
-//     const listDragContent = $('.list-drag');
-//     if (listDragContent.length) {
-
-//         const list = document.querySelector('.list-drag')
-//         const listItems = document.querySelectorAll('.list-item')
-//         const listHidden = document.querySelector('.list-hidden')
-        
-//         // let dragIndex, dragSource
-    
-//         const getMouseOffset = (evt) => {
-//         const targetRect = evt.target.getBoundingClientRect()
-//         const offset = {
-//             // x: evt.pageX - targetRect.left,
-//             y: evt.pageY - targetRect.top
-//         }
-//         return offset
-//         }
-    
-//         const getElementVerticalCenter = (el) => {
-//         const rect = el.getBoundingClientRect()
-//         return (rect.bottom - rect.top) / 2
-//         }
-    
-//         const appendPlaceholder = (evt, idx) => {
-//         evt.preventDefault()
-//         if (idx === dragIndex) {
-//             return
-//         }
-        
-//         const offset = getMouseOffset(evt)
-//         const middleY = getElementVerticalCenter(evt.target)
-//         const placeholder = list.children[dragIndex]
-        
-//         // console.log(`hover on ${idx} ${offset.y > middleY ? 'bottom half' : 'top half'}`)
-//         if (offset.y > middleY) {
-//             list.insertBefore(evt.target, placeholder)
-//         } else if (list.children[idx + 1]) {
-//             list.insertBefore(evt.target.nextSibling || evt.target, placeholder)
-//         }
-//         return
-//         }
-    
-//         function sortable(rootEl, onUpdate) {
-//         var dragEl;
-        
-//         // Making all siblings movable
-//         [].slice.call(rootEl.children).forEach(function (itemEl) {
-//             itemEl.draggable = true;
-//         });
-        
-//         // Function responsible for sorting
-//         function _onDragOver(evt) {
-//             evt.preventDefault();
-//             evt.dataTransfer.dropEffect = 'move';
-            
-//             var target = evt.target;
-//             if (target && target !== dragEl && target.nodeName == 'LI') {
-//                 // Sorting
-//             const offset = getMouseOffset(evt)
-//             const middleY = getElementVerticalCenter(evt.target)
-    
-//             if (offset.y > middleY) {
-//                 rootEl.insertBefore(dragEl, target.nextSibling)
-//             } else {
-//                 rootEl.insertBefore(dragEl, target)
-//             }
-//             }
-//         }
-        
-//         // End of sorting
-//         function _onDragEnd(evt){
-//             evt.preventDefault();
-            
-//             dragEl.classList.remove('ghost');
-//             rootEl.removeEventListener('dragover', _onDragOver, false);
-//             rootEl.removeEventListener('dragend', _onDragEnd, false);
-    
-    
-//             // Notification about the end of sorting
-//             onUpdate(dragEl);
-//         }
-        
-//         // Sorting starts
-//         rootEl.addEventListener('dragstart', function (evt){
-//             dragEl = evt.target; // Remembering an element that will be moved
-            
-//             // Limiting the movement type
-//             evt.dataTransfer.effectAllowed = 'move';
-//             evt.dataTransfer.setData('Text', dragEl.textContent);
-    
-    
-//             // Subscribing to the events at dnd
-//             rootEl.addEventListener('dragover', _onDragOver, false);
-//             rootEl.addEventListener('dragend', _onDragEnd, false);
-    
-    
-//             setTimeout(function () {
-//                 // If this action is performed without setTimeout, then
-//                 // the moved object will be of this class.
-//                 dragEl.classList.add('ghost');
-//             }, 0)
-//         }, false);
-//         }
-                            
-//         // Using                    
-//         sortable(list, function (item) {
-//         // console.log(item);
-//         });
-//     }
-
-// }
